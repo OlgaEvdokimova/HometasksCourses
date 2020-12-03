33ead8a45386367e95d619bcc4af69a16f109203
Olga <olgaaevdokim@gmail.com>
2020-12-03 15:52:59 +0300

    urlThead

---
+++ src/com/company/UrlClass.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.function.Consumer;
+
+public class UrlClass {
+    private String url = "https://ru.wikipedia.org/wiki/%D0%97%D0%B0%D0%B3%D0%BB%D0%B0%D0%B2%D0%BD%D0%B0%D1%8F_%D1%81%D1%82%D1%80%D0%B0%D0%BD%D0%B8%D1%86%D0%B0";
+
+    Consumer<Byte[]> lambda = (it) -> System.out.println()
+    public UrlClass(String url) {
+        this.url = url;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void start(){
+
+    }
+
+}
